67207e61e51fa004bc0cc72d5c1b8bd73c2e3403
Dmitry <borodulkin_d@mail.ru>
2022-06-14 15:52:35 +0300

    Partially tabular representation.

---
+++ Home Work 2.7/ViewControllers/ContactListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TableViewController.swift
+//  Home Work 2.7
+//
+//  Created by Дмитрий Бородулькин on 14.06.2022.
+//
+
+import UIKit
+
+class ContactListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+         10
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath)
+
+
+
+        return cell
+    }
+    
+
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
