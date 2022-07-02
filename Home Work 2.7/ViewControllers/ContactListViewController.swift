2b656acc6c0fb68a5db126a18d340e3fae3e0dae
Dmitry <borodulkin_d@mail.ru>
2022-07-02 18:56:42 +0300

    Modified the logic in the ContactListViewController file and placed it in the ViewControllers group.

--- Home Work 2.7/ViewControllers/ContactListViewController.swift
+++ Home Work 2.7/ViewControllers/ContactListViewController.swift
@@ -9,39 +9,30 @@ import UIKit
 
 class ContactListViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+    var persons: [Person] = []
 
     // MARK: - Table view data source
-
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-         10
+        persons.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-
-
+        let person = persons[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = person.fullName
+        cell.contentConfiguration = content
+        
         return cell
     }
-    
 
-  
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let indexPath = tableView.indexPathForSelectedRow {
+            guard let detailVC = segue.destination as? ContactDetailsViewController else { return }
+            detailVC.person = persons[indexPath.row]
+        }
     }
-    */
-
 }
