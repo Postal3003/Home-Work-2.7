d92479dec5c1707d43aa4916c27e75f5c73db403
Dmitry <borodulkin_d@mail.ru>
2022-07-02 17:55:09 +0300

    Created a TabBarViewController file and placed it in the ViewControllers group.

---
+++ Home Work 2.7/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarViewController.swift
+//  Home Work 2.7
+//
+//  Created by Дмитрий Бородулькин on 02.07.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
+        guard let contactListVC = viewControllers?.first as? ContactListViewController else { return }
+        guard let sectionVC = viewControllers?.last as? SectionTableViewController else { return }
+        
+        contactListVC.persons = persons
+        sectionVC.persons = persons
+    }
+}
